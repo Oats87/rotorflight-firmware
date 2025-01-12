4122441da8aee629f36c18d4a656a9841c053e00
Chris Kim <oats87g@gmail.com>
2025-01-12 10:05:49 -0800

    Change governor P_TERM_LIMIT to 0.50f
    Signed-off-by: Chris Kim <oats87g@gmail.com>

--- src/main/flight/governor.c
+++ src/main/flight/governor.c
@@ -70,7 +70,7 @@
 #define GOV_NOMINAL_CELL_VOLTAGE        3.70f
 
 // PID term limits
-#define GOV_P_TERM_LIMIT                0.20f
+#define GOV_P_TERM_LIMIT                0.50f
 #define GOV_I_TERM_LIMIT                0.95f
 #define GOV_D_TERM_LIMIT                0.20f
 #define GOV_F_TERM_LIMIT                0.50f
